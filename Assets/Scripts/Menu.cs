ccd4aacb4328a590c745de5034ea702ca203102e
SonMV <maivietson.nd@gmail.com>
2020-02-27 08:24:41 +0700

    Implement function for button D1

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,15 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadFirstLevel()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(1);
     }
 }
