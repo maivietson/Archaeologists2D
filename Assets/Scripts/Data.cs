02c7569be3cee06fee4aae0d384ce94cf3f2508b
son.maiviet@gameloft.com <son.maiviet@gameloft.com>
2020-03-02 18:32:41 +0700

    Save data scene D1

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class LevelData
+{
+    public List<CropData> cropData = new List<CropData>();
+}
+
+[Serializable]
+public class CropData
+{
+    public TransformData transformData = new TransformData(0, 0);
+    public string itemScriptableObject = "";
+}
+
+[Serializable]
+public class TransformData
+{
+    public TransformData(float _x, float _y)
+    {
+        x = _x;
+        y = _y;
+    }
+
+    public float x;
+    public float y;
+}
---
+++ Assets/Scripts/LoadSaveData.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LoadSaveData : MonoBehaviour
+{
+    private void wake()
+    {
+        int numLoadSaveData = FindObjectsOfType<LoadSaveData>().Length;
+        if(numLoadSaveData > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Saving()
+    {
+        //List<Transform> placesObjects = transform.GetComponentsInChildren<Transform>().ToList();
+        List<Transform> placesObjects = GameObject.Find("Pickups").transform.GetComponentsInChildren<Transform>().ToList();
+        placesObjects.Remove(transform);
+
+        LevelData data = new LevelData();
+
+        foreach(Transform t in placesObjects)
+        {
+            CropData cropData = new CropData();
+            cropData.transformData = new TransformData(t.localPosition.x, t.localPosition.y);
+            cropData.itemScriptableObject = t.name;
+
+            data.cropData.Add(cropData);
+        }
+    }
+}
