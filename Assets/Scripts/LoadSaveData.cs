db718b931082b2cfe6a8962fddf3fb4d01aed8c1
son.maiviet@gameloft.com <son.maiviet@gameloft.com>
2020-03-03 18:19:01 +0700

    Load/Save scene D2

--- Assets/Scripts/LoadSaveData.cs
+++ Assets/Scripts/LoadSaveData.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadSaveData : MonoBehaviour
 {
+    int currentSceneIndex;
+    GameObject target;
+
     private void wake()
     {
         int numLoadSaveData = FindObjectsOfType<LoadSaveData>().Length;
@@ -21,7 +27,10 @@ public class LoadSaveData : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        target = GameObject.Find("Pickups");
+        //SaveGame();
+        LoadGame();
     }
 
     // Update is called once per frame
@@ -30,10 +39,10 @@ public class LoadSaveData : MonoBehaviour
         
     }
 
-    public void Saving()
+    private LevelData CreateSaveGameObject()
     {
         //List<Transform> placesObjects = transform.GetComponentsInChildren<Transform>().ToList();
-        List<Transform> placesObjects = GameObject.Find("Pickups").transform.GetComponentsInChildren<Transform>().ToList();
+        List<Transform> placesObjects = target.transform.GetComponentsInChildren<Transform>().ToList();
         placesObjects.Remove(transform);
 
         LevelData data = new LevelData();
@@ -43,8 +52,50 @@ public class LoadSaveData : MonoBehaviour
             CropData cropData = new CropData();
             cropData.transformData = new TransformData(t.localPosition.x, t.localPosition.y);
             cropData.itemScriptableObject = t.name;
-
+            print("Name of object: " + t.name);
             data.cropData.Add(cropData);
+        }
+
+        return data;
+    }
+
+    public void SaveGame()
+    {
+        // create save
+        LevelData save = CreateSaveGameObject();
+
+        // binary data
+        BinaryFormatter bf = new BinaryFormatter();
+        string fileName = "level" + currentSceneIndex + ".save";
+        FileStream file = File.Create(Application.persistentDataPath + "/" + fileName);
+        bf.Serialize(file, save);
+        file.Close();
+    }
+
+    public void LoadGame()
+    {
+        // handle file
+        string fileName = "level" + currentSceneIndex + ".save";
+        if (File.Exists(Application.persistentDataPath + "/" + fileName))
+        {
+            //read file
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/" + fileName, FileMode.Open);
+            LevelData save = (LevelData)bf.Deserialize(file);
+            file.Close();
+
+            //handle data
+            foreach(CropData data in save.cropData)
+            {
+                print("Name: " + data.itemScriptableObject);
+                print("Position: " + data.transformData.x + ", " + data.transformData.y);
+                //GameObject instanceCoin = Instantiate(Resources.Load("Prefabs/Coin"), new Vector3(data.transformData.x, data.transformData.y, 0f), Quaternion.identity) as GameObject;
+                //instanceCoin.transform.SetParent(target.transform);
+            }
+        }
+        else
+        {
+            Debug.Log("No game saved!");
         }
     }
 }
