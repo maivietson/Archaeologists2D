9239f0abeadb78cf387513bc8c6c55326b68fdf7
Sonmv <maivietson.nd@gmail.com>
2020-05-27 02:18:48 +0700

    [son.mv] Implement addressable for platform

---
+++ Assets/Scripts/PrepareDataScript/DataStructure.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class Data
+{
+    public List<Level> listLevel = new List<Level>();
+}
+
+[Serializable]
+public class Level
+{
+    public string nameLevel;
+    public string objectName;
+    public List<Position> listPosition = new List<Position>();
+}
+
+[Serializable]
+public class Position
+{
+    public float posX;
+    public float posY;
+    public float posZ;
+}
